36135128caff980c77c161cbc13b4c43cbfa0a32
Quincey ten Kroode <qjv.tenkroode@gmail.com>
2024-08-12 15:18:24 +0200

    commit dangling changes

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -8,7 +8,7 @@ return {
 				sources = {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.gofmt,
-					null_ls.builtins.formatting.goimports,
+                    null_ls.builtins.formatting.goimports,
                     null_ls.builtins.diagnostics.staticcheck,
 				},
 			})
